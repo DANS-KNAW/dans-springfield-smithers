1ee151de4acc5d11b58d589ba4e45f3dce4c1300
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2018-07-24 22:16:01 +0200

    WIP

--- src/com/noterik/bart/fs/LazyHomer.java
+++ src/com/noterik/bart/fs/LazyHomer.java
@@ -255,45 +255,20 @@ public class LazyHomer implements MargeObserver {
 	/**
 	 * Initializes logger
 	 */
-    private void initLogger() {    	 
-    	System.out.println("Initializing logging for smithers");
+    private void initLogger() {
+			System.out.println("SMITHERS: initializing logging...");
     	
-    	// get logging path
-    	String logPath = LazyHomer.getRootPath().substring(0,LazyHomer.getRootPath().indexOf("webapps"));
-			logPath += "logs/smithers/smithers.log";
-
-			File xmlConfig = new File("/springfield/smithers/log4j.xml");
+			File xmlConfig = new File("/springfield/smithers2/log4j.xml");
 			if (xmlConfig.exists()) {
-				System.out.println("Uter: reading logging config from XML file at " + xmlConfig);
+				System.out.println("Smithers: reading logging config from XML file at " + xmlConfig);
 				DOMConfigurator.configure(xmlConfig.getAbsolutePath());
 				LOG.info("Logging configured from file: " + xmlConfig);
 			} else {
-
-				try {
-					// default layout
-					Layout layout = new PatternLayout("%-5p: %d{yyyy-MM-dd HH:mm:ss} %c %x - %m%n");
-
-					// rolling file appender
-					DailyRollingFileAppender appender1 = new DailyRollingFileAppender(layout, logPath,
-							"'.'yyyy-MM-dd");
-					BasicConfigurator.configure(appender1);
-
-					// console appender
-					ConsoleAppender appender2 = new ConsoleAppender(layout);
-					BasicConfigurator.configure(appender2);
-				}
-				catch (IOException e) {
-					System.out.println("SmithersServer got an exception while initializing the logger.");
-					e.printStackTrace();
-				}
-
-				Level logLevel = Level.INFO;
-				Logger.getRootLogger().setLevel(Level.OFF);
-				Logger.getLogger(PACKAGE_ROOT).setLevel(logLevel);
-				LOG.info("logging level: " + logLevel);
+			  System.out.println("SMITHERS: ");
 			}
 		
 		LOG.info("Initializing logging done.");
+			System.out.println("Smithers works with logger repository: " + LogManager.getLoggerRepository());
     }
 
 	
